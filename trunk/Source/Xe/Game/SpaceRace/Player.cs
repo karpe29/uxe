de511cf7d0b7cfaa34339b2047282f0c44ee1423
jbriguet <jbriguet@6195f892-b531-0410-91e0-437e01270c33>
2007-06-05 22:37:13 +0000

    Added Ship Types and ChaseCamera (camera still need integration)

--- trunk/Source/Xe/Game/SpaceRace/Player.cs
+++ trunk/Source/Xe/Game/SpaceRace/Player.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using XeFramework.XeGame.SpaceRace;
+using XeFramework.Graphics3D;
 
 namespace Xe.Game.SpaceRace
 {
 	class Player
 	{
 		Ship m_ship;
+
+		ChaseCamera m_camera;
+
+
 	}
 }
